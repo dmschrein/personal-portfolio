64aef20ff9ab786001a6cd64c9b29bbe8cdcac89
dmschrein <darrelmschreiner@gmail.com>
2024-06-11 13:09:22 -0700

    updated footer

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { FaHome } from "react-icons/fa"
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Approach from "@/components/Approach"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
